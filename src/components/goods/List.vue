924e75e7412b13b7706a1de1a03c9fc6c9f8491f
吴炫强 <1503030731@qq.com>
2021-09-11 14:07:03 +0800

    完成了订单功能的开发

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -33,7 +33,7 @@
         </el-table-column>
         <el-table-column label="操作" width="130px">
           <template slot-scope="scope">
-            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row.goods_id)"></el-button>
             <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeById(scope.row.goods_id)"></el-button>
           </template>
         </el-table-column>
@@ -42,6 +42,24 @@
       <!-- 分页区域 -->
       <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryInfo.pagenum" :page-sizes="[5, 10, 15, 20]" :page-size="queryInfo.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total" background> </el-pagination>
     </el-card>
+
+    <el-dialog title="修改用户" :visible.sync="editDialogVisible" width="50%" @close="editDialogClosed">
+      <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="100px">
+        <el-form-item label="商品名称" prop="goods_name">
+          <el-input v-model="editForm.goods_name"></el-input>
+        </el-form-item>
+        <el-form-item label="商品价格" prop="goods_price">
+          <el-input v-model="editForm.goods_price"></el-input>
+        </el-form-item>
+        <el-form-item label="商品重量" prop="goods_weight">
+          <el-input v-model="editForm.goods_weight"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editGoodsInfo">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -58,7 +76,14 @@ export default {
       // 商品列表
       goodslist: [],
       // 总数据条数
-      total: 0
+      total: 0,
+      editDialogVisible: false,
+      editForm: {},
+      editFormRules: {
+        goods_name: [{ required: true, message: '请输入商品名称', trigger: 'blur' }],
+        goods_price: [{ required: true, message: '请输入商品价格', trigger: 'blur' }],
+        goods_weight: [{ required: true, message: '请输入商品重量', trigger: 'blur' }]
+      }
     }
   },
   created() {
@@ -96,6 +121,25 @@ export default {
     },
     goAddpage() {
       this.$router.push('/goods/add')
+    },
+    async showEditDialog(id) {
+      const { data: res } = await this.$http.get('goods/' + id)
+      if (res.meta.status !== 200) return this.$message.error('查询商品信息失败！')
+      this.editForm = res.data
+      this.editDialogVisible = true
+    },
+    editDialogClosed() {
+      this.$refs.editFormRef.resetFields()
+    },
+    editGoodsInfo() {
+      this.$refs.editFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.put('goods/' + this.editForm.goods_id, { goods_name: this.editForm.goods_name, goods_price: this.editForm.goods_price, goods_weight: this.editForm.goods_weight, goods_number: this.editForm.goods_number, goods_cat: this.editForm.goods_cat })
+        if (res.meta.status !== 200) this.$message.error('更新商品信息失败！')
+        this.editDialogVisible = false
+        this.getGoodsList()
+        this.$message.success('更新商品信息成功！')
+      })
     }
   }
 }
